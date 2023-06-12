e21650b6321de8da5d8fe9d6683c5539a19fa6b3
Quinoku <joaquinrzmz@gmail.com>
2023-06-12 12:12:19 +0200

    Mejora modo oscuro en pagina principal

--- cambioVisualizacion.js
+++ cambioVisualizacion.js
@@ -1,5 +1,6 @@
 const boton = document.getElementById("modooscuro");
 
+let imagenInicio = document.getElementById("imageninicio");
 let estilos = document.getElementById("hojaestilos");
 
 boton.addEventListener("click", () => {
@@ -7,10 +8,12 @@ boton.addEventListener("click", () => {
     if(estilos.href.includes("CSS/estilos.css")){
 
         estilos.href = "CSS/estilosOsc.css";
+        imagenInicio.src = "IMG/mobile-01-01OSCURO.webp";
 
     } else if(estilos.href.includes("CSS/estilosOsc.css")) {
 
         estilos.href = "CSS/estilos.css";
+        imagenInicio.src = "IMG/mobile-01-01CLARO.webp";
 
     }
 });
